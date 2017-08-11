f6bb5139627a8518d913bc6dd57e19136e30eea6
Hugo Mota <hugo.iguanaa@gmail.com>
2017-08-10 23:18:42 -0300

    Exercícios

--- src/br/com/caelum/jdbc/ConectionFactory.java
+++ src/br/com/caelum/jdbc/ConectionFactory.java
@@ -9,8 +9,11 @@ public class ConectionFactory {
 	public Connection getConection() {
 
 		try {
+			Class.forName("org.postgresql.Driver");	
 			return DriverManager.getConnection("jdbc:postgresql://localhost/fj21", "postgres", "admin");
 		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} catch (ClassNotFoundException e) {
 			throw new RuntimeException(e);
 		}
 	}
--- src/br/com/caelum/servlet/AdicionaContatoServlet.java
+++ src/br/com/caelum/servlet/AdicionaContatoServlet.java
@@ -2,6 +2,7 @@ package br.com.caelum.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.caelum.jdbc.ConectionFactory;
 import br.com.caelum.jdbc.dao.ContatoDao;
 import br.com.caelum.jdbc.modelo.Contato;
 
@@ -35,21 +37,24 @@ public class AdicionaContatoServlet extends HttpServlet {
 		String nome = request.getParameter("nome");
 		String endereco = request.getParameter("endereco");
 		String email = request.getParameter("email");
-		String dataEmTexto = request.getParameter("dataNascimento");
+		String dataEmTexto = request.getParameter("data_nascimento");
 		Calendar dataNascimento = null;
 
 		// fazendo a conversão da data
-		try {
+		if(dataEmTexto != null){
+			try {
 
-			Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
-			dataNascimento = Calendar.getInstance();
-			dataNascimento.setTime(date);
+				Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
+				dataNascimento = Calendar.getInstance();
+				dataNascimento.setTime(date);
 
-		} catch (ParseException e) {
-			out.println("Erro de conversão da data");
-			return; // para a execução do método
+			} catch (ParseException e) {
+				out.println("Erro de conversão da data");
+				return; // para a execução do método
+			}
+	
 		}
-
+		
 		// monta um objeto contato
 		Contato contato = new Contato();
 		contato.setNome(nome);
@@ -57,6 +62,9 @@ public class AdicionaContatoServlet extends HttpServlet {
 		contato.setEmail(email);
 		contato.setDataNascimento(dataNascimento);
 
+		Connection con = new ConectionFactory().getConection();
+		
+		
 		// salva o contato
 		ContatoDao dao = new ContatoDao();
 		dao.adiciona(contato);
